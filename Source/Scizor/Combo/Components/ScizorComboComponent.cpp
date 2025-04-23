2f1406bfddc2748fe37debd5087b49534400cb18
tarnishablec <tarnishablec@outlook.com>
2025-04-23 16:09:01 +0800

    Clean Code

--- Source/Scizor/Combo/Components/ScizorComboComponent.cpp
+++ Source/Scizor/Combo/Components/ScizorComboComponent.cpp
@@ -7,7 +7,7 @@
 #include "Scizor/Combo/Animation/AnimNotifyState_ScizorComboWindow.h"
 #include "Scizor/Combo/Schema/ScizorComboSchema.h"
 
-UE_DEFINE_GAMEPLAY_TAG(Tag_StateTreeEvent_BachComboInput, "StateTreeEvent.Bach.Combo.ComboInput");
+UE_DEFINE_GAMEPLAY_TAG(Tag_StateTreeEvent_BachComboInput, "Scizor.Combo.ComboInput");
 
 namespace Scizor
 {
