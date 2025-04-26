3905359f608d0b046cf2c16a8f0b3c9f4af64493
tarnishablec <tarnishablec@outlook.com>
2025-04-26 23:19:36 +0800

    Gengar Trigger Ability With Callback

---
+++ Source/Scizor/Combo/Helpers/ScizorComboLibrary.h
@@ -0,0 +1,18 @@
+﻿// Copyright 2019-Present tarnishablec. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ScizorComboLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCIZOR_API UScizorComboLibrary : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+};
---
+++ Source/Scizor/Combo/Nodes/Tasks/ScizorPlayComboMontage.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright 2019-Present tarnishablec. All Rights Reserved.
+
+
+#include "ScizorPlayComboMontage.h"
+
+#if  WITH_EDITOR
+TArray<FString> UScizorPlayComboMontage::GetSections() const
+{
+    TArray<FString> Result{};
+    if (Montage)
+    {
+        for (auto&& CompositeSection : Montage->CompositeSections)
+        {
+            Result.AddUnique(CompositeSection.SectionName.ToString());
+        }
+    }
+
+    return Result;
+}
+#endif
